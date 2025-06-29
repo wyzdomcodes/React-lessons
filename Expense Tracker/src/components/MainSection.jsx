24912e943937db15c14d8bea233e78f339d58712
wyzdomcodes <aguowisdom@gmail.com>
2025-06-29 16:14:49 +0100

    add tag to transaction and refactor code

--- Expense Tracker/src/components/MainSection.jsx
+++ Expense Tracker/src/components/MainSection.jsx
@@ -2,20 +2,32 @@ import balanceImage from "../assets/balance-img.png";
 import TransactionHistory from "./TransactionHistory";
 import { useState } from "react";
 export default function MainSection() {
-  const [transactions, setTransactions] = useState([
-    // 
-  ])
+  const [transactions, setTransactions] = useState([{description: "Salary", amount: 1200, date: "11-06-2025", type: "income"},
+    {description: "Food", amount: -200, date: "11-06-2025", type: "expense"},
+    {description: "rent", amount: -100, date: "11-06-2025", type: "expense"},
+  ]);
 
   function handleAddTransaction(formData) {
-      const description = formData.get("description")
-      const amount = formData.get("amount")
-      const date = formData.get("date")
-    setTransactions(prev => [...prev, {description: description, amount: Number(amount)}])
-    }
-  const balance = transactions.reduce((acc, t) => acc + t.amount, 0)
-  const income = transactions.filter((t) => t.amount > 0).reduce((acc, t) => acc + t.amount, 0)
-  const expenses = transactions.filter((t) => t.amount < 0)
-  const totalExpenses = Math.abs(expenses.reduce((acc, t) => acc + t.amount, 0))
+    const { description, amount, date, type } = Object.fromEntries(formData);
+    let parsedAmount = Number(amount);
+  if (type === "expense") {
+    parsedAmount = -Math.abs(parsedAmount); // Ensure it's negative
+  } else {
+    parsedAmount = Math.abs(parsedAmount); // Ensure it's positive
+  }
+    setTransactions((prev) => [
+      ...prev,
+      { description, amount: parsedAmount, date, type }
+    ]);
+  }
+  const balance = transactions.reduce((acc, t) => acc + t.amount, 0);
+  const income = transactions
+    .filter((t) => t.amount > 0)
+    .reduce((acc, t) => acc + t.amount, 0);
+  const expenses = transactions.filter((t) => t.amount < 0);
+  const totalExpenses = Math.abs(
+    expenses.reduce((acc, t) => acc + t.amount, 0),
+  );
   return (
     <main className="lg:px-42 md:px-21 sm:px-10.5 py-5">
       <div className="">
@@ -29,67 +41,92 @@ export default function MainSection() {
         <div className="p-4 flex justify-between gap-4">
           <div className="bg-[#F0F2F5] flex-1 p-6 rounded-lg">
             <h3 className="text-sm/6 text-[#121217]">Income</h3>
-            <p className="text-lg font-bold text-[#121217]">${income > 0 ? income : "$0.00"}</p>
+            <p className="text-lg font-bold text-[#121217]">
+              {income > 0 ? '$' + income : "$0.00"}
+            </p>
           </div>
           <div className="bg-[#F0F2F5] flex-1 p-6 rounded-lg">
             <h3 className="text-sm/6 text-[#121217]">Expenses</h3>
-            <p className="text-lg font-bold text-[#121217]">${totalExpenses > 0 ? totalExpenses : "$0.00"}</p>
+            <p className="text-lg font-bold text-[#121217]">
+              {totalExpenses > 0 ? '$'+ totalExpenses : "$0.00"}
+            </p>
           </div>
         </div>
-        <TransactionHistory transactions={transactions}/>
+        <TransactionHistory transactions={transactions} />
         <div className="px-4 pt-5 pb-3">
-        <h2 className="text-[22px]/7 font-bold text-[#121217]">
-          Add Transaction
-        </h2>
-      </div>
+          <h2 className="text-[22px]/7 font-bold text-[#121217]">
+            Add Transaction
+          </h2>
+        </div>
 
-      <form action={handleAddTransaction} id="add-transaction">
-        <div className="px-4 py-3 flex flex-col max-w-120">
-          <label
-            htmlFor="description"
-            className="text-base/6 text-[#121217] font-medium pb-2"
-          >
-            Description
-          </label>
-          <input
-            type="text"
-            name="description"
-            placeholder="Enter Description"
-            className="outline-none border rounded-lg p-3.5 border-[#DBDBE5] placeholder:text-[#636687] placeholder:text-base/6"
-          />
-        </div>
-        <div className="px-4 py-3 flex flex-col max-w-120">
-          <label
-            htmlFor="description"
-            className="text-base/6 text-[#121217] font-medium pb-2"
-          >
-            Amount
-          </label>
-          <input
-            type="number"
-            name="amount"
-            placeholder="Enter Amount"
-            className="outline-none border rounded-lg p-3.5 border-[#DBDBE5] placeholder:text-[#636687] placeholder:text-base/6"
-          />
-        </div>
-        <div className="px-4 py-3 flex flex-col max-w-120">
-          <label
-            htmlFor="date"
-            className="text-base/6 text-[#121217] font-medium pb-2"
-          >
-            Date
-          </label>
-          <input
-            type="date"
-            name="date"
-            placeholder="Enter Date"
-            className="outline-none border rounded-lg p-3.5 border-[#DBDBE5] placeholder:text-[#636687] placeholder:text-base/6"
-          />
-        </div>
-        <div className="py-3 px-4 flex justify-end items-center">
-          <button type="submit" className="cursor-pointer h-10 px-4 rounded-lg bg-[#616BE8] font-bold text-sm/5.5 text-[#FFFFFF]">Add trasaction</button>
-        </div>
-      </form>
+        <form action={handleAddTransaction} id="add-transaction">
+          <div className="px-4 py-3 flex flex-col max-w-120">
+            <label
+              htmlFor="description"
+              className="text-base/6 text-[#121217] font-medium pb-2"
+            >
+              Description
+            </label>
+            <input
+              type="text"
+              name="description"
+              placeholder="Enter Description"
+              className="outline-none border rounded-lg p-3.5 border-[#DBDBE5] placeholder:text-[#636687] placeholder:text-base/6"
+            />
+          </div>
+          <div className="px-4 py-3 flex flex-col max-w-120">
+            <label
+              htmlFor="description"
+              className="text-base/6 text-[#121217] font-medium pb-2"
+            >
+              Amount
+            </label>
+            <input
+              type="number"
+              name="amount"
+              placeholder="Enter Amount"
+              className="outline-none border rounded-lg p-3.5 border-[#DBDBE5] placeholder:text-[#636687] placeholder:text-base/6"
+            />
+          </div>
+          <div className="px-4 py-3 flex flex-col max-w-120">
+            <label
+              htmlFor="date"
+              className="text-base/6 text-[#121217] font-medium pb-2"
+            >
+              Date
+            </label>
+            <input
+              type="date"
+              name="date"
+              placeholder="Enter Date"
+              className="outline-none border rounded-lg p-3.5 border-[#DBDBE5] placeholder:text-[#636687] placeholder:text-base/6"
+            />
+          </div>
+          <div className="px-4 py-3 flex flex-col max-w-120">
+            <label
+              htmlFor="faveColor"
+              className="text-base/6 text-[#121217] font-medium pb-2"
+            >
+              Select Transaction Type?
+            </label>
+            <select
+              id="type"
+              name="type"
+              className="focus:outline-none border rounded-lg p-3.5 border-[#DBDBE5] placeholder:text-[#636687] placeholder:text-base/6"
+            >
+              <option value="income">Income</option>
+              <option value="expense">Expense</option>
+            </select>
+          </div>
+          <div className="py-3 px-4 flex justify-end items-center">
+            <button
+              type="submit"
+              className="cursor-pointer h-10 px-4 rounded-lg bg-[#616BE8] font-bold text-sm/5.5 text-[#FFFFFF]"
+            >
+              Add trasaction
+            </button>
+          </div>
+        </form>
       </div>
     </main>
   );
--- Expense Tracker/src/components/TransactionHistory.jsx
+++ Expense Tracker/src/components/TransactionHistory.jsx
@@ -1,43 +1,67 @@
 import History from "./History";
 import wallet from "../assets/wallet.png";
-export default function TransactionHistory({transactions}) {
+export default function TransactionHistory({ transactions }) {
   return (
     <>
       <div className="px-4 pt-5 pb-3">
         <h2 className="text-[22px]/7 font-bold text-[#121217]">History</h2>
       </div>
-      {transactions.length === 0 && <History>
-        <div className="flex flex-col justify-center items-center gap-4">
-          <img src={wallet} alt="wallet image" />
-          <h3 className="font-bold text-[18px]/[23px] text-[#121217]">
-            No transactions yet
-          </h3>
-          <p className="text-[14px]/[21px]">
-            Add a trasaction to start tracking your expenses
-          </p>
-          <a
-            href="#add-transaction"
-            className="bg-[#F0F2F5] px-4 py-2 rounded-[8px] text-[#121217] text-[14px]/[21px]"
-          >
-            Add Transaction
-          </a>
-        </div>
-      </History>}
-      {transactions.length > 0 && <History>
-        {transactions.map(transaction => {
-          return (
-            <div key={transaction.amount} className="flex justify-between items-center py-2 border-b border-b-[#DBDBE5]">
-          <div className="">
-            <p className="text-[#121217] text-base/6 font-medium">{transaction.description}</p>
-            <span className="inline-block text-[#636687] text-sm/[21px]">{transaction.date}</span>
+      {transactions.length === 0 && (
+        <History>
+          <div className="flex flex-col justify-center items-center gap-4">
+            <img src={wallet} alt="wallet image" />
+            <h3 className="font-bold text-[18px]/[23px] text-[#121217]">
+              No transactions yet
+            </h3>
+            <p className="text-[14px]/[21px]">
+              Add a trasaction to start tracking your expenses
+            </p>
+            <a
+              href="#add-transaction"
+              className="bg-[#F0F2F5] px-4 py-2 rounded-[8px] text-[#121217] text-[14px]/[21px]"
+            >
+              Add Transaction
+            </a>
           </div>
-          <div className="">
-            <p className="text-[#121217] text-base/6">-${String(transaction.amount).slice(1)}</p>
-          </div>
-        </div>
-          )
-        })}
-      </History>}
+        </History>
+      )}
+      {transactions.length > 0 && (
+        <History>
+          {transactions.map((transaction) => {
+            return (
+              <div
+                key={transaction.amount}
+                className="flex justify-between items-center py-2 border-b border-b-[#DBDBE5]"
+              >
+                <div className="">
+                  <p className="text-[#121217] text-base/6 font-medium">
+                    {transaction.description}{" "}
+                    <span
+                      className={
+                        transaction.type === "income"
+                          ? "text-[12px] text-white bg-green-600 py-0.5 px-1 rounded"
+                          : "text-[12px] text-white bg-red-600 py-0.5 px-1 rounded"
+                      }
+                    >
+                      {transaction.type}
+                    </span>
+                  </p>
+                  <span className="inline-block text-[#636687] text-sm/[21px]">
+                    {transaction.date}
+                  </span>
+                </div>
+                <div className="">
+                  <p className="text-[#121217] text-base/6">
+                    {String(transaction.amount).includes("-")
+                      ? `-$${String(transaction.amount).slice(1)}`
+                      : `$${String(transaction.amount)}`}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </History>
+      )}
     </>
   );
 }
