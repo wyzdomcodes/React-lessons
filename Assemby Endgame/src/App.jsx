a711263333fb27a4e411877af8e268c1a499e7cc
wyzdomcodes <aguowisdom@gmail.com>
2025-07-05 14:19:08 +0100

    bug fix

--- Assemby Endgame/src/App.jsx
+++ Assemby Endgame/src/App.jsx
@@ -1,26 +1,38 @@
 import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import { languages } from "./components/languages";
+import { words } from "./components/words";
 import { clsx } from "clsx";
+import { useWindowSize } from "react-use";
+import Confetti from "react-confetti";
 import { getFarewellText } from "./components/utils";
+
 function App() {
-  const [cureentWord, setCurrentWord] = useState("React");
+  const [cureentWord, setCurrentWord] = useState(getNewWord);
   const [guessedAlphabet, setGuessedAlphabet] = useState([]);
   const [guessedWord, setGuessedWord] = useState([]);
-  const [wrongGuessedWord, setWrongGuessedWord] = useState([])
-  const [isGameOver, setIsGameOver] = useState(false)
-  const wrongGuessCount = wrongGuessedWord.length
+  const [wrongGuessedWord, setWrongGuessedWord] = useState([]);
+  const [isGameOver, setIsGameOver] = useState(false);
+  const wrongGuessCount = wrongGuessedWord.length;
   const alphabets = "abcdefghijklmnopqrstuvwxyz";
-  const gameWon = cureentWord.toUpperCase().split('').every(letter => guessedAlphabet.includes(letter))
+  const gameWon = cureentWord
+    .toUpperCase()
+    .split("")
+    .every((letter) => guessedAlphabet.includes(letter));
+  const { width, height } = useWindowSize();
   let correctAlphabet;
-  let [farewellText, setFarewellText] = useState(null)
+  let [farewellText, setFarewellText] = useState(null);
+  function getNewWord() {
+    let random = Math.floor(Math.random() * words.length);
+    return words[random];
+  }
+
   function handleNewGame() {
-    let random = Math.ceil(Math.random() * 8)
-    setCurrentWord(prev => prev = languages[random].name)
-    setGuessedAlphabet([])
-    setGuessedWord([])
-    setWrongGuessedWord([])
-    setIsGameOver([])
+    getNewWord();
+    setGuessedAlphabet([]);
+    setGuessedWord([]);
+    setWrongGuessedWord([]);
+    setIsGameOver(false);
   }
   const alphabetElements = alphabets
     .toUpperCase()
@@ -28,7 +40,7 @@ function App() {
     .map((alphabet, index) => {
       return (
         <button
-        disabled={isGameOver}
+          disabled={isGameOver}
           key={index}
           onClick={() => storeGuessedGame(alphabet)}
           className={clsx(
@@ -38,7 +50,9 @@ function App() {
                 ? "bg-green-600"
                 : "bg-red-600"
               : "bg-yellow-600",
-              isGameOver ? "opacity-45 cursor-default" : "cursor-pointer opacity-100"
+            isGameOver
+              ? "opacity-45 cursor-default"
+              : "cursor-pointer opacity-100",
           )}
         >
           {alphabet}
@@ -53,37 +67,52 @@ function App() {
         return [...prev, alphabet];
       }
     });
-     correctAlphabet = cureentWord.toUpperCase().includes(alphabet);
+    correctAlphabet = cureentWord.toUpperCase().includes(alphabet);
     setGuessedWord((prev) =>
-      correctAlphabet && !prev.includes(alphabet) ? [...prev, alphabet] : prev
+      correctAlphabet && !prev.includes(alphabet) ? [...prev, alphabet] : prev,
     );
     const wrongAlphabet = !cureentWord.toUpperCase().includes(alphabet);
-    setWrongGuessedWord(prev => wrongAlphabet && !wrongGuessedWord.includes(alphabet) ? [...prev, alphabet] : prev)
+    setWrongGuessedWord((prev) =>
+      wrongAlphabet && !wrongGuessedWord.includes(alphabet)
+        ? [...prev, alphabet]
+        : prev,
+    );
 
-    const l = languages.map(lang => getFarewellText(lang.name))
-    setFarewellText(prev => !correctAlphabet ? l[wrongGuessCount] : prev )
-    
+    const l = languages.map((lang) => getFarewellText(lang.name));
+    setFarewellText((prev) => (!correctAlphabet ? l[wrongGuessCount] : prev));
   }
-  useEffect(() =>  {
-    setIsGameOver(wrongGuessCount === 8 ? true : false)
-  }, [wrongGuessCount])
+  useEffect(() => {
+    setIsGameOver(wrongGuessCount === 8 ? true : false);
+  }, [wrongGuessCount]);
   function text() {
-    if(!correctAlphabet && !gameWon && !isGameOver && wrongGuessCount > 0) {
-      return farewellText
+    if (!correctAlphabet && !gameWon && !isGameOver && wrongGuessCount > 0) {
+      return farewellText;
     }
   }
   function displayFarewell() {
-    if(!correctAlphabet && !gameWon && !isGameOver && wrongGuessCount > 0) {
-      return "bg-purple-900"
-    } else return
+    if (!correctAlphabet && !gameWon && !isGameOver && wrongGuessCount > 0) {
+      return "bg-purple-900";
+    } else return;
   }
+
   return (
     <>
       <Header />
       <main className="flex flex-col justify-center items-center py-10">
-        <div className={clsx("rounded flex justify-center items-center min-w-70 h-15 px-3", isGameOver && "bg-red-700", gameWon && "bg-emerald-500", displayFarewell())}>
-          <h3 className={"text-lg font-bold text-center"}>{isGameOver && "Opps You lost"} {gameWon && "You Won"} {text()}</h3>
+        <div
+          className={clsx(
+            "rounded flex justify-center items-center min-w-70 h-15 px-3",
+            isGameOver && "bg-red-700",
+            gameWon && "bg-emerald-500",
+            displayFarewell(),
+          )}
+        >
+          <h3 className={"text-lg font-bold text-center"}>
+            {isGameOver && "Opps You lost"} {gameWon && "You Won"} {text()}
+          </h3>
         </div>
+
+        {gameWon && <Confetti width={width} height={height} />}
 
         <div className="flex flex-wrap justify-center gap-3 py-8 max-w-[500px]">
           {languages.map((language, index) => {
@@ -94,8 +123,9 @@ function App() {
                   background: language.backgroundColor,
                   color: language.color,
                 }}
-                className={clsx("rounded py-2 px-4",
-                  index < wrongGuessCount && 'opacity-35' 
+                className={clsx(
+                  "rounded py-2 px-4",
+                  index < wrongGuessCount && "opacity-35",
                 )}
               >
                 {language.name}
@@ -105,23 +135,36 @@ function App() {
         </div>
 
         <div className="flex justify-center items-center gap-0.5">
-          {guessedWord.map((word, index) => {
-            return (
-              <span
-                key={index}
-                className="bg-gray-800 shadow-2xl border-b-3 inline-block font-bold text-lg px-4 py-1"
-              >
-                {word.toUpperCase()}
-              </span>
-            );
-          })}
+          {cureentWord
+            .toUpperCase()
+            .split("")
+            .map((letter, index) => {
+              return (
+                <span
+                  key={index}
+                  className={clsx(
+                    "w-10 h-12 flex items-center justify-center border-b-4 border-gray-600 bg-[#1E1E1E] text-2xl font-bold",
+                  )}
+                >
+                  {guessedAlphabet.includes(letter) ? letter : ""}
+                </span>
+              );
+            })}
         </div>
 
         <div className="flex flex-wrap justify-center gap-x-1 gap-y-2 py-8 max-w-[400px]">
           {alphabetElements}
         </div>
 
-        {isGameOver || gameWon && <button onClick={handleNewGame} type="button" className="bg-[#0cc2da] text-[#1E1E1E] px-5 py-2">New Game</button>}
+        {(isGameOver || gameWon) && (
+          <button
+            onClick={handleNewGame}
+            type="button"
+            className="bg-[#0cc2da] text-[#1E1E1E] px-5 py-2 cursor-pointer"
+          >
+            New Game
+          </button>
+        )}
       </main>
     </>
   );
